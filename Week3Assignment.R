70a9d6c46cd9e45a9f967ba5297ccc32c3932473
atpgarciaa <82021854+atpgarciaa@users.noreply.github.com>
2021-04-06 15:55:41 +0800

    Add files via upload

---
+++ Week3Assignment.R
@@ -0,0 +1,25 @@
+makeCacheMatrix <- function(k = matrix()) {
+  inv <- NULL
+  set <- function(n){
+    k <<- n
+    inv <<- NULL
+  }
+  get <- function() {k}
+  setInverse <- function(inversematrix) inv <<- inversematrix
+  getInverse <- function() inv
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+}
+
+
+cacheSolve <- function(k, ...) {
+  
+  inv <- k$getInverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  matrix <- k$get()
+  inv <- solve(matrix, ...)
+  k$setInverse(inv)
+  inv   
+}
