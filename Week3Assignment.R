e15c2bcdf1cb0bb6cdfdffc51563ef745b43aaee
atpgarciaa <82021854+atpgarciaa@users.noreply.github.com>
2021-04-07 11:52:14 +0800

    Update Week3Assignment.R

--- Week3Assignment.R
+++ Week3Assignment.R
@@ -1,22 +1,30 @@
-makeCacheMatrix <- function(k = matrix()) {
+makeCacheMatrix <- function(k = matrix()) 
+{
   inv <- NULL
   set <- function(n){
     k <<- n
     inv <<- NULL
+    
   }
+ 
   get <- function() {k}
   setInverse <- function(inversematrix) inv <<- inversematrix
   getInverse <- function() inv
-  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+  list(set = set, 
+       get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
 }
 
 
-cacheSolve <- function(k, ...) {
+
+cacheSolve <- function(k, ...) 
   
+{
   inv <- k$getInverse()
   if(!is.null(inv)){
     message("getting cached data")
-    return(inv)
+    return(inv)    
   }
   matrix <- k$get()
   inv <- solve(matrix, ...)
